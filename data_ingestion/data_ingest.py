ecb3fb7b32080433dfa29a805809738bb7c47e6a
debdoot9804 <debdutsen111@gmail.com>
2025-04-27 16:15:42 +0530

    setup added

---
+++ data_ingestion/data_ingest.py
@@ -0,0 +1,12 @@
+from langchain_astradb import AstraDBVectorStore
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from data_ingestion.data_transform import data_convert
+
+
+class ingest_data:
+    def __init__(self):
+        pass
+    def data_ingestion(self):
+        pass
---
+++ data_ingestion/data_transform.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from langchain_core.documents import Document
+
+class data_convert:
+    def __init__(self):
+        pass
+
+    def data_transform(self):
+        pass
+
+    
